ff2a5fb015ed61048f9635fdba26fa5a8b22728c
Onionbao <411965697@qq.com>
2019-01-24 21:55:37 +0800

    Update LIVEFolder.py

--- LIVEFolder.py
+++ LIVEFolder.py
@@ -11,12 +11,10 @@ import random
 
 
 def getFileName(path, suffix):
-    ''' 获取指定目录下的所有指定后缀的文件名 '''
     filename = []
     f_list = os.listdir(path)
     # print f_list
     for i in f_list:
-        # os.path.splitext():分离文件名与扩展名
         if os.path.splitext(i)[1] == suffix:
             filename.append(i)
     return filename
